2e339953b4e02da0d61716208a342b2a25c50fd0
hammasali7 <hamsimirza1@gmail.com>
2021-06-03 19:13:43 +0500

    first commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Strings
+const String kAppTitle = "Pet Care";
+
+// Numbers
+const double kDefaultPadding = 16;
+
+// Colors
+const Color kDefaultBackgroundColor = Color(0XFFF5EEE8);
